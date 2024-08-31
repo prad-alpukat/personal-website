df59438dfa24a13236e66b0e18fc66503e76f4c5
prad-alpukat <pradityaaldi.s@gmail.com>
2024-08-31 20:57:06 +0700

    refactor: Update portfolio page URL and fix typos

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -49,7 +49,7 @@ export function PortofolioPost() {
           <Link
             key={post.slug}
             className="flex flex-col space-y-1 mb-4"
-            href={`/blog/${post.slug}`}
+            href={`/portofolio/${post.slug}`}
           >
             <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
               <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
